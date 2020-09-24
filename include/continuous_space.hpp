f95afea46eb1e89b1ac4a10d478f142658931e5c
ppanchal <piyush.panchal@sam.math.ethz.ch>
2020-09-24 11:08:42 +0200

    Initial commit

---
+++ include/continuous_space.hpp
@@ -0,0 +1,165 @@
+/**
+ * \file continuous_space.hpp
+ * \brief This file declares a templated class inherited from AbstractBEMSpace
+ *        and represents discontinuous spaces of the form \f$S^{0}_{p}\f$
+ *        as defined in \f$\eqref{eq:Sp}\f$, using full template specialization.
+ *
+ * This File is a part of the 2D-Parametric BEM package
+ */
+
+#ifndef CONTSPACEHPP
+#define CONTSPACEHPP
+
+#include "abstract_bem_space.hpp"
+
+#include <utility>
+#include <vector>
+
+#include <Eigen/Dense>
+#include "abstract_parametrized_curve.hpp"
+#include "parametrized_mesh.hpp"
+
+namespace parametricbem2d {
+/**
+ * \class ContinuousSpace
+ * \brief This templated class inherits from the class AbstractBEMSpace
+ *        . This class implements the BEM spaces of the form \f$S^{0}_{p}\f$
+ *        defined in \f$\eqref{eq:Sp}\f$. The class is implemented through full
+ * template specialization for different values of p.
+ */
+template <unsigned int p> class ContinuousSpace : public AbstractBEMSpace {
+public:
+  ContinuousSpace() {
+    std::cout << "Error! No specialization defined for p = " << p << std::endl;
+  }
+};
+
+/**
+ * \brief This is a specialization of the templated class for p = 1.
+ *        This class represents the space \f$S^{0}_{1}\f$
+ */
+template <> class ContinuousSpace<1> : public AbstractBEMSpace {
+public:
+  // Local to Global Map
+  unsigned int LocGlobMap(unsigned int q, unsigned int n,
+                          unsigned int N) const {
+    // Asserting the index of local shape function and the panel number are
+    // within limits
+    assert(q <= q_ && n <= N);
+    if (q == 2)
+      return n;
+    else
+      return (n % N == 0) ? 1 : (n + 1);
+  }
+
+  // Space Dimensions as defined in \f$\ref{T:thm:dimbe}\f$
+  unsigned int getSpaceDim(unsigned int numpanels) const {
+    return numpanels * (q_ - 1);
+  }
+
+  // Function for interpolating the input function
+  Eigen::VectorXd Interpolate(const std::function<double(double, double)> &func,
+                              const ParametrizedMesh &mesh) const {
+    // The output vector
+    unsigned numpanels = mesh.getNumPanels();
+    unsigned coeffs_size = getSpaceDim(numpanels);
+    Eigen::VectorXd coeffs(coeffs_size);
+    // using PanelVector = parametricbem2d::PanelVector;
+    // Filling the coefficients
+    for (unsigned i = 0; i < coeffs_size; ++i) {
+      Eigen::Vector2d pt = mesh.getVertex(i);
+      coeffs(i) = func(pt(0), pt(1));
+    }
+    return coeffs;
+  }
+
+  // Constructor
+  ContinuousSpace() {
+    // Number of reference shape functions for the space
+    q_ = 2;
+    // Reference shape function 1, defined using a lambda expression
+    BasisFunctionType b1 = [&](double t) { return 0.5 * (t + 1); };
+    // Reference shape function 2, defined using a lambda expression
+    BasisFunctionType b2 = [&](double t) { return 0.5 * (1 - t); };
+    // Adding the reference shape functions to the vector
+    referenceshapefunctions_.push_back(b1);
+    referenceshapefunctions_.push_back(b2);
+    // Reference shape function 1 derivative, defined using a lambda expression
+    BasisFunctionType b1dot = [&](double t) { return 0.5; };
+    // Reference shape function 2 derivative, defined using a lambda expression
+    BasisFunctionType b2dot = [&](double t) { return -0.5; };
+    // Adding the reference shape function derivatives to the vector
+    referenceshapefunctiondots_.push_back(b1dot);
+    referenceshapefunctiondots_.push_back(b2dot);
+  }
+};
+
+/**
+ * \brief This is a specialization of the templated class for p = 2.
+ *        This class represents the space \f$S^{0}_{2}\f$
+ */
+template <> class ContinuousSpace<2> : public AbstractBEMSpace {
+public:
+  unsigned int LocGlobMap(unsigned int q, unsigned int n,
+                          unsigned int N) const {
+    // Asserting the index of local shape function and the panel number are
+    // within limits
+    assert(q <= q_ && n <= N);
+    if (q == 2)
+      return n;
+    else if (q == 1)
+      return (n % N == 0) ? 1 : (n + 1);
+    else
+      return N + n;
+  }
+
+  // Space Dimensions as defined in \f$\ref{T:thm:dimbe}\f$
+  unsigned int getSpaceDim(unsigned int numpanels) const {
+    return numpanels * (q_ - 1);
+  }
+
+  // Function for interpolating the input function
+  Eigen::VectorXd Interpolate(const std::function<double(double, double)> &func,
+                              const ParametrizedMesh &mesh) const {
+    // The output vector
+    unsigned numpanels = mesh.getNumPanels();
+    unsigned coeffs_size = getSpaceDim(numpanels);
+    Eigen::VectorXd coeffs(coeffs_size);
+    // using PanelVector = parametricbem2d::PanelVector;
+    // Filling the coefficients
+    for (unsigned i = 0; i < coeffs_size; ++i) {
+      // TODO
+      coeffs(i) = 0;
+    }
+    return coeffs;
+  }
+
+  // Constructor
+  ContinuousSpace() {
+    // Number of reference shape functions for the space
+    q_ = 3;
+    // Reference shape function 1, defined using a lambda expression
+    BasisFunctionType b1 = [&](double t) { return 0.5 * (t + 1); };
+    // Reference shape function 2, defined using a lambda expression
+    BasisFunctionType b2 = [&](double t) { return 0.5 * (1 - t); };
+    // Reference shape function 3, defined using a lambda expression
+    BasisFunctionType b3 = [&](double t) { return 1 - t * t; };
+    // Adding the reference shape functions to the vector
+    referenceshapefunctions_.push_back(b1);
+    referenceshapefunctions_.push_back(b2);
+    referenceshapefunctions_.push_back(b3);
+    // Reference shape function 1 derivative, defined using a lambda expression
+    BasisFunctionType b1dot = [&](double t) { return 0.5; };
+    // Reference shape function 2 derivative, defined using a lambda expression
+    BasisFunctionType b2dot = [&](double t) { return -0.5; };
+    // Reference shape function 3 derivative, defined using a lambda expression
+    BasisFunctionType b3dot = [&](double t) { return -2 * t; };
+    // Adding the reference shape function derivatives to the vector
+    referenceshapefunctiondots_.push_back(b1dot);
+    referenceshapefunctiondots_.push_back(b2dot);
+    referenceshapefunctiondots_.push_back(b3dot);
+  }
+};
+} // namespace parametricbem2d
+
+#endif // CONTSPACEHPP
---
+++ include/gauleg.hpp
@@ -0,0 +1,80 @@
+/**
+ * \file gauleg.hpp
+ * \brief This file defines the function to evaluate points and weights for
+ *        Gauss Legendre quadrature rule.
+ *
+ * This File is a part of the 2D-Parametric BEM package
+ */
+#ifndef GAULEGHPP
+#define GAULEGHPP
+
+#include "logweight_quadrature.hpp"
+#include <Eigen/Dense>
+#include <cmath>
+#include <iostream>
+#include <cmath>
+#define _USE_MATH_DEFINES
+
+/* @brief Compute Gaussian quadrature nodes and weights for n nodes over
+ * interval [a,b] \param[in] a,b Interval [a,b] endpoints \param[in] n Number of
+ * quadrature points \param[out] xq,wq Pair of Gauss-Legendre quadrature points
+ * and weights
+ */
+inline std::pair<Eigen::RowVectorXd, Eigen::RowVectorXd>
+gauleg(double a, double b, int n, double eps = 1.e-13) {
+  int i, j, m;
+  double xmid, xlen, p1, p2, p3, dp1, z, z1, wqi;
+  Eigen::RowVectorXd xq(n), wq(n);
+
+  m = (n + 1) / 2;
+  xmid = 0.5 * (a + b);
+  xlen = 0.5 * (b - a);
+
+  // get roots
+  for (i = 0; i < m; i++) {
+
+    // i-th root guess
+    z = std::cos(M_PI * (i + 1 - 0.25) / (n + 0.5));
+
+    // get i-th root
+    do {
+      p1 = 1.;
+      p2 = 0.;
+      for (j = 1; j <= n; j++) {
+        p3 = p2;
+        p2 = p1;
+        p1 = ((2. * j - 1.) * z * p2 - (j - 1.) * p3) / j;
+      }
+      dp1 = n * (z * p1 - p2) / (z * z - 1.0);
+      z1 = z;
+      z = z1 - p1 / dp1;
+    } while (std::abs(z - z1) > eps);
+
+    // set nodes
+    xq(i) = xmid - xlen * z;
+    xq(n - 1 - i) = xmid + xlen * z;
+
+    // set weights
+    wqi = 2. * xlen / ((1. - z * z) * dp1 * dp1);
+    wq(i) = wqi;
+    wq(n - 1 - i) = wqi;
+  }
+
+  // return
+  return std::make_pair(xq, wq);
+}
+
+inline QuadRule getGaussQR(unsigned N) {
+  // Getting standard Gauss Legendre Quadrature weights and nodes
+  Eigen::RowVectorXd weights, points;
+  std::tie(points, weights) =
+      gauleg(-1, 1, N, std::numeric_limits<double>::epsilon());
+  QuadRule gauss;
+  gauss.dim = 1;
+  gauss.n = N;
+  gauss.x = points;
+  gauss.w = weights;
+  return gauss;
+}
+
+#endif
